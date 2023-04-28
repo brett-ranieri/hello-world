37d30606e98458f15ec1b2f7d6ad771365079862
Brett Ranieri <brett.ranieri@gmail.com>
2023-04-28 15:37:31 -0600

    integrate GiftedChat, minor styling

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,75 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ navigation, route }) => {
 	const { name } = route.params;
 	const { background } = route.params;
+	const [messages, setMessages] = useState([]);
 
 	useEffect(() => {
 		navigation.setOptions({ title: name }); //sets Username to title on use of component
+		setMessages([
+			{
+				_id: 1,
+				text: "Hello developer",
+				createdAt: new Date(),
+				user: {
+					_id: 2,
+					name: "React Native",
+					avatar: "https://placeimg.com/140/140/any",
+				},
+			},
+			{
+				_id: 2,
+				text: name + " has entered the chat",
+				createdAt: new Date(),
+				system: true,
+			},
+		]);
 	}, [navigation, name]);
 
+	const onSend = (newMessages) => {
+		setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
+	};
+
+	const renderBubble = (props) => {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					right: {
+						backgroundColor: "#9ca0a8",
+					},
+					left: {
+						backgroundColor: "#F3F4F6",
+					},
+				}}
+				textStyle={{
+					right: {
+						color: "#F3F4F6",
+					},
+				}}
+			/>
+		);
+	};
+
 	return (
-		<View style={[styles.container, { backgroundColor: background }]}>
-			<Text
-				style={{ color: background == "#090C08" || background == "#474056" ? "#ffffff" : "black" }}
-			>
-				I'm the Chat page!
-			</Text>
+		<View
+			style={[
+				styles.container,
+				{ backgroundColor: background },
+				{ color: background == "#090C08" || background == "#474056" ? "#ffffff" : "black" },
+			]}
+		>
+			<GiftedChat
+				messages={messages}
+				onSend={(messages) => onSend(messages)}
+				user={{
+					_id: 1,
+				}}
+				renderBubble={renderBubble}
+			/>
 		</View>
 	);
 };
@@ -23,8 +77,6 @@ const Chat = ({ navigation, route }) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: "space-evenly",
-		alignItems: "center",
 	},
 });
 
