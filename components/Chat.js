bb52c2dac82cc57d9918e1cfa0c741d3d5f34cf1
Brett Ranieri <brett.ranieri@gmail.com>
2023-04-28 15:56:50 -0600

    add keyboard avoiding view for android

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ navigation, route }) => {
@@ -70,6 +70,7 @@ const Chat = ({ navigation, route }) => {
 				}}
 				renderBubble={renderBubble}
 			/>
+			{Platform.OS === "android" ? <KeyboardAvoidingView behavior='height' /> : null}
 		</View>
 	);
 };
